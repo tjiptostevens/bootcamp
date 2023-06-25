73660b0e5e0654ce8e4b805eadcf1b6a3099bda2
tjiptostevens <steveneonites@gmail.com>
2023-06-25 21:12:37 +0700

    edit navbar

--- src/website/home.jsx
+++ src/website/home.jsx
@@ -1,9 +1,7 @@
 import Hero from "./hero";
 import "../assets/css/home.css";
-import Promo from "./promo";
 import Program from "./program";
 import Footer from "./footer";
-import WhyUs from "./whyus";
 import Contact from "./contact";
 import Faq from "./faq";
 import Fact from "./fact";
