cea1c1d75178fa3b8a6583bccc9876ad630b2fd6
tjiptostevens <steveneonites@gmail.com>
2023-07-09 23:08:02 +0700

    edit footer

--- src/website/footer.jsx
+++ src/website/footer.jsx
@@ -1,10 +1,14 @@
 import "../assets/css/footer.css";
+import { logo } from "../custom/img";
 const Footer = () => {
   return (
     <>
       <div className="w-100">
         <div className="__copyright">
-          copyright 2023. bekal<b>kerja</b>. | <i>+6283846864486</i>
+          copyright 2023.{" "}
+          <img src={logo} style={{ height: "20px", margin: "0 0 0 0.5rem" }} />.{" "}
+          <span style={{ margin: "0 1rem" }}>|</span>
+          <i>+6283846864486</i>
         </div>
       </div>
     </>
